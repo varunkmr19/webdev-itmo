02c83332decd73d0f9dd0026d82441af7d8f9c07
varunkmr19 <varun-kumar@live.com>
2021-01-03 09:04:11 +0530

    hide secret key

--- django_project_kunal/django_project_kunal/settings.py
+++ django_project_kunal/django_project_kunal/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9%+1bphkgy2b4tw9%=vc*)vapd-5+^0l(hz__lsmd(%c^111(d'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
