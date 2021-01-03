deeaabf060ea66bd71bcaf4d8ff9c9fec5880377
varunkmr19 <varun-kumar@live.com>
2021-01-03 08:51:14 +0530

    add detail view and url mapping

--- django_project_kunal/project_first_app/views.py
+++ django_project_kunal/project_first_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import Http404
+
+from . models import Possession, CarOwner
 
 # Create your views here.
+
+
+def detail(request, owner_id):
+    try:
+        owner = CarOwner.objects.get(pk=owner_id)
+        possessions = Possession.objects.filter(owner=owner).all()
+    except CarOwner.DoesNotExist:
+        raise Http404("Owner does not exist")
+    except Possession.DoesNotExist:
+        raise Http404("Possession does not exist")
+
+    return render(request, 'project_first_app/owner.html', {
+        'owner': owner,
+        'possessions': possessions
+    })
