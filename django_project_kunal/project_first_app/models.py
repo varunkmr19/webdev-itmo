f1e4ac247e530869a9a9dff2a9c01d25c82bd083
varunkmr19 <varun-kumar@live.com>
2021-01-04 08:26:40 +0530

    changed all DateTimeField to DateTime

--- django_project_kunal/project_first_app/models.py
+++ django_project_kunal/project_first_app/models.py
@@ -1,5 +1,7 @@
+from datetime import date
 from django.db import models
 from django.db.models.fields.related import ForeignKey
+from django.utils.timezone import now
 
 # Create your models here.
 
@@ -7,20 +9,20 @@ from django.db.models.fields.related import ForeignKey
 class CarOwner(models.Model):
     last_name = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
-    date_of_birth = models.DateTimeField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
 
     def __str__(self):
-        return self.last_name
+        return f'{self.last_name} {self.name}'
 
 
 class DriverIdentity(models.Model):
     driver = models.ForeignKey(CarOwner, on_delete=models.CASCADE)
     number_identification = models.CharField(max_length=10)
     type = models.CharField(max_length=10)
-    date_of_issue = models.DateTimeField()
+    date_of_issue = models.DateField(default=now)
 
     def __str__(self):
-        return self.driver.last_name
+        return f'{self.last_name} {self.name}'
 
 
 class Car(models.Model):
@@ -37,8 +39,8 @@ class Possession(models.Model):
     owner = ForeignKey(CarOwner, on_delete=models.CASCADE,
                        related_name='owner')
     car = ForeignKey(Car, on_delete=models.CASCADE, related_name='car')
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField(null=True, blank=True)
+    start_date = models.DateField(default=date(1997, 4, 10))
+    end_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
-        return self.owner.last_name
+        return f'{self.owner.last_name} - {self.car.model}'
