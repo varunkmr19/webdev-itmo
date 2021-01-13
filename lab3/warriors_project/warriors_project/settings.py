8401d6c3f193c4de30ec6ab2738031d408634590
varunkmr19 <varun-kumar@live.com>
2021-01-13 13:55:11 +0530

    hotel project initial setup

--- lab3/warriors_project/warriors_project/settings.py
+++ lab3/warriors_project/warriors_project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "v%#461)@7g_3s^45f%-+w3_4=n-4pl(#ezzhyu#1f^wdi_)&cw"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
